6ffa20a5daf20145000242cf082d18a04f11ab62
Zamur Borges Vedovatto <vedovatto@gmail.com>
2022-07-08 22:46:49 -0300

    adjust

--- src/sender.ts
+++ src/sender.ts
@@ -7,13 +7,21 @@ class Sender {
         this.initialize()
     }
 
+    async sendText(to:string, body: string) {
+        // 5531988382148@c.us
+        // 553181061339@c.us
+        console.log("TO", to)
+        await this.client.sendText(to, body)
+    }
+
     private initialize() {
         const qr = (base64Qrimg:string) => {}
         const status = (statusSession:string) => {
             //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || initWhatsapp || erroPageWhatsapp || successPageWhatsapp || waitForLogin || waitChat || successChat
         }
-        const start = (client: Whatsapp) => {
+        const start = async (client: Whatsapp) => {
             this.client = client
+            await this.sendText("553188382148@c.us", "Olá, tudo bem?")
         }
 
 
